666067c0b7d53127f0ed1268b5326d7dc945ef78
sushant <sushantchoudhari@gmail.com>
2024-05-24 21:36:17 +0100

    125. Valid Palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -3,7 +3,8 @@ package src;
 public class Palindrome {
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        validPalindrome(s);
+       boolean res= validPalindrome(s);
+        System.out.println(" res "+ res);
     }
 
     private static boolean validPalindrome(String s) {
@@ -17,7 +18,12 @@ public class Palindrome {
         System.out.println(" " + fixedString);
         int a_pointer=0;
         int b_pointer=fixedString.length()-1;
+        while (fixedString.charAt(a_pointer) != fixedString.charAt(b_pointer )) {
+            return false;
+        }
+        a_pointer +=1;
+        b_pointer -=1;
 
-        return false;
+        return true;
     }
 }
